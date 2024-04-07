2289cf69281420e3561a02968811e17ae3e0cf9c
Olle Jörlid <a21olljo@student.his.se>
2024-04-07 14:40:59 +0200

    Day 1 done (I think)

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+class CalibrationDocumnet
+{
+    private static IEnumerable<int> ExtractDigits(IEnumerable<string> lines)
+    {
+        List<int> digits = new List<int>();
+
+        foreach (string line in lines) 
+        {
+            var digit = new StringBuilder();
+            digit.Append(0);
+            foreach (char c in line)
+            {
+                if (Char.IsDigit(c))
+                {
+                    digit.Append(c);
+                    break;
+                }
+            }
+
+            char[] enil = line.ToCharArray();
+            Array.Reverse(enil);
+            foreach (char c in enil) 
+            {
+                if (Char.IsDigit(c))
+                {
+                    digit.Append(c);
+                    break;
+                }
+            }
+
+            digits.Add(Int32.Parse(digit.ToString()));
+        }
+
+        return digits;
+    } 
+
+    public static void Main(string[] args)
+    {
+        IEnumerable<string> inputs = File.ReadLines("C:\\Users\\olljo\\source\\repos\\AdventOfCode2023\\Day1\\Day1_inputs.txt");
+
+        IEnumerable<int> digits = ExtractDigits(inputs);
+
+        int sum = digits.Sum();
+
+        Console.WriteLine(sum);
+    }
+}
