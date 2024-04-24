abbbded30ef899e76c4ce9fb1cd120786c085dfb
Olle Jörlid <a21olljo@student.his.se>
2024-04-24 20:29:37 +0200

    Done kinda, I forgot about its ability to squize between pipes. And that it can go diagonally. However, if that weren't a possibility then this would work, and it is pretty :)

--- Day10/Day10Main.cs
+++ Day10/Day10Main.cs
@@ -16,6 +16,7 @@ class Day10Main
         Console.WriteLine(PipeSystem.pipeSystem.Count + " "+ PipeSystem.pipeSystem.First().Count);
 
         int nrNests = GetNests();
+        Console.WriteLine(nrNests);
     }
 
     private static int GetNests()
@@ -27,79 +28,22 @@ class Day10Main
             {
                 if (traversedMap[i, j] == '\0')
                 {
-                    try
-                    {
-                        if (GetEscape(i, j))
+                    DfsEscape de = new DfsEscape();
+
+                        if (de.TryToEscape(i, j))
                         {
                             traversedMap[i, j] = '.';
                             counter++;
                         }
-                    }
-                    catch (Exception) { }
                 }
 
                 if (traversedMap[i, j] != '\0')
-                {
                     Console.Write(traversedMap[i, j]);
-                }
                 else
-                {
                     Console.Write(" ");
-                }
-
             }
             Console.WriteLine();
         }
         return counter;
-    }
-
-    private static bool GetEscape(int index, int jndex)
-    {
-        bool valid = false;
-
-        for (int i = index; i < PipeSystem.pipeSystem.Count; i++)
-        {
-            if (traversedMap[i, jndex] != '\0')
-            {
-                valid = true;
-                break;
-            }
-        }
-        if (!valid) return false;
-
-        valid = false;
-        for (int i = index; i != 0; i--)
-        {
-            if (traversedMap[i, jndex] != '\0')
-            {
-                valid = true;
-                break;
-            }
-        }
-        if (!valid) return false;
-
-        valid = false;
-        for (int j = jndex; j < PipeSystem.pipeSystem.Count; j++)
-        {
-            if (traversedMap[index, j] != '\0')
-            {
-                valid = true;
-                break;
-            }
-        }
-        if (!valid) return false;
-
-        valid = false;
-        for (int j = jndex; j != 0; j--)
-        {
-            if (traversedMap[index, j] != '\0')
-            {
-                valid = true;
-                break;
-            }
-        }
-        if (!valid) return false;
-        
-        return true;
     }
 }
---
+++ Day10/DfsEscape.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10;
+
+/*  Solution "inspired" from GFGs solution 
+*   https://www.geeksforgeeks.org/depth-first-traversal-dfs-on-a-2d-array/
+*/
+
+internal class DfsEscape
+{
+    static int ROW = PipeSystem.pipeSystem.Count;
+    static int COL = PipeSystem.pipeSystem.First().Count;
+
+    // Initialize direction vectors
+    static int[] dRow = { 0, 1, 0, -1 };
+    static int[] dCol = { -1, 0, 1, 0 };
+    
+    internal bool TryToEscape(int row, int col)
+    {
+        int startRow = row;
+        int startCol = col;
+
+        char[,] grid = Day10Main.traversedMap;
+
+        bool[,] vis = new bool[ROW, COL];
+        for (int i = 0; i < PipeSystem.pipeSystem.Count; i++)
+        {
+            for (int j = 0; j < PipeSystem.pipeSystem.First().Count; j++)
+            {
+                vis[i, j] = false;
+            }
+        }
+
+        return DFS(startRow, grid, startCol, vis);
+    }
+
+    private bool DFS(int row, char[,] grid, int col, bool[,] vis)
+    {
+        // Initialize a stack of pairs and
+        // push the starting cell into it
+        Stack st = new Stack();
+        st.Push(new Tuple<int, int>(row, col));
+
+        // Iterate until the
+        // stack is not empty
+        while (st.Count > 0)
+        {
+            // Pop the top pair
+            Tuple<int, int> curr = (Tuple<int, int>)st.Peek();
+            st.Pop();
+
+            row = curr.Item1;
+            col = curr.Item2;
+
+            // If cell is out of bounds
+            if (row < 0 || col < 0 ||
+                row >= ROW || col >= COL)
+                return false;
+
+            // Checks if its a wall
+            if (grid[row, col] != '\0')
+                continue;
+
+            // Check if the current popped
+            // cell is a valid cell or not
+            if (vis[row, col])
+                continue;
+
+            // Mark the current
+            // cell as visited
+            vis[row, col] = true;
+
+            // Push all the adjacent cells
+            for (int i = 0; i < 4; i++)
+            {
+                int adjx = row + dRow[i];
+                int adjy = col + dCol[i];
+                st.Push(new Tuple<int, int>(adjx, adjy));
+            }
+        }
+        return true;
+    }
+}
