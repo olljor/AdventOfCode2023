ed7a1788c10d9f27f89e737f3f523606b0c46ae0
Olle Jörlid <a21olljo@student.his.se>
2024-04-26 11:29:55 +0200

    Done with Day 13. Had to end early yesterday after about 1.5h of work. However, one wrong assumption and a missing "=" sign cost me about 2h today, but it did make me redesign everything better and document my thoughts so thats good :)

---
+++ Day13/PatternHandler.cs
@@ -0,0 +1,57 @@
+﻿namespace Day13;
+
+class Pattern
+{
+    public List<string> horizontal = new List<string>();
+    public List<string> vertical = new List<string>();
+}
+
+internal class PatternHandler
+{
+    public static List<Pattern> patterns = new List<Pattern>();
+    public PatternHandler(IEnumerable<string> inputs)
+    {
+        List<string> tempPattern = new List<string>();
+        foreach (var input in inputs)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                patterns.Add(SetRealPattern(tempPattern));
+                tempPattern = new List<string>();
+            }
+            else
+            {
+                tempPattern.Add(input);
+            }
+        }
+        patterns.Add(SetRealPattern(tempPattern));
+    }
+
+    private Pattern SetRealPattern(List<string> tempPattern)
+    {
+        List<string> localHori = new List<string>();
+        List<string> localVert = new List<string>();
+
+        for (int i = 0; i < tempPattern.Count; i++)
+        {
+            localHori.Add(tempPattern[i]);
+
+        }
+
+        for (int j = 0; j < tempPattern.First().Length; j++)
+        {
+            List<char> chars = new List<char>();
+            for (int i = 0; i < tempPattern.Count; i++)
+            {
+                chars.Add(tempPattern[i][j]);
+            }
+            localVert.Add(new string(chars.ToArray()));
+        }
+
+        return new Pattern
+        {
+            horizontal = localHori,
+            vertical = localVert
+        };
+    }
+}
