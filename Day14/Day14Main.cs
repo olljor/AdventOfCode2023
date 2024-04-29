9c4e60b865072a1db464ae6b732b67b355d085bc
Olle Jörlid <a21olljo@student.his.se>
2024-04-29 22:01:26 +0200

    Day 14 done

--- Day14/Day14Main.cs
+++ Day14/Day14Main.cs
@@ -1,11 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿using Day14;
 /*
  * Ze Plan:
- * Platform handler which sets up the platform as a list of lists of strings
+ * Platform handler which sets up the platform as a lists of strings
  * Where every string is the the column of the platform (input)
  *      We are only interested in the vertical lines because we are only tilting the platform north    
  * When platform set up, tilt platform by moving every O to the index+1 of previous # or O
  * Then calculate weight by taking the list.count - index+1 of every O 
  */
+
+class Day14Main
+{
+    public static async Task Main()
+    {
+        IEnumerable<string> inputs = File.ReadLines("C:\\Users\\olljo\\source\\repos\\AdventOfCode2023\\Day14\\Day14_input.txt");
+        PlatformHandler platformHandler = new PlatformHandler();
+
+        List<string> verticalPlatform = platformHandler.SetUpPlatform(inputs);
+        List<string> tiltedVerticalPlatform = await platformHandler.TiltVerticalPlatform(verticalPlatform);
+
+        int weight = 0;
+        foreach (string verticalPattern in tiltedVerticalPlatform)
+        {
+            Console.WriteLine(verticalPattern);
+            for (int i = 0; i < verticalPattern.Length; i++)
+            {
+                if (verticalPattern[i] == 'O')
+                {
+                    weight = weight + verticalPattern.Length - i;
+                }
+            }
+        }
+        Console.WriteLine(weight);
+    }
+}
---
+++ Day14/PlatformHandler.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14;
+class PlatformHandler
+{
+    public List<string> SetUpPlatform(IEnumerable<string> inputs)
+    {
+        List<string> verticalPatterns = new List<string>();
+        List<string> inputList = inputs.ToList();
+        for (int i = 0; i < inputList.First().Length; i++)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < inputList.Count; j++)
+            {
+                sb.Append(inputList[j][i]);
+            }
+            verticalPatterns.Add(sb.ToString());
+        }
+        return verticalPatterns;
+    }
+
+    public async Task<List<string>> TiltVerticalPlatform(List<string> verticalPlatform)
+    {
+        List<Task<string>> tasks = new List<Task<string>>();
+
+        foreach (string pattern in verticalPlatform)
+        {
+            tasks.Add(Task.Run(() => RollBoulders(pattern)));
+        }
+        await Task.WhenAll(tasks);
+
+        List<string> tiltedVerticalPatterns = new List<string>();
+        tasks.ForEach(task => tiltedVerticalPatterns.Add(task.Result));
+
+        return tiltedVerticalPatterns;
+    }
+
+    private string RollBoulders(string pattern)
+    {
+        char[] chars = pattern.ToCharArray();
+        bool boulderMoved = false;
+        while (!boulderMoved)
+        {
+            bool boulderFound = false;
+            boulderMoved = true;
+            for (int i = chars.Length - 1; i >= 0; i--)
+            {
+                if ((chars[i] == '#' || chars[i] == 'O')
+                    && boulderFound)
+                {
+                    chars[i + 1] = 'O';
+                    boulderFound = false;
+                }
+
+                if (chars[i] == 'O' && !boulderFound)
+                {
+                    boulderFound = true;
+                    chars[i] = '.';
+                }
+                else
+                {
+                    if (boulderFound)
+                    {
+                        boulderMoved = false;
+                    }
+                    chars[i] = chars[i];
+                }
+            }
+            if (boulderFound)
+            {
+                chars[0] = 'O';
+            }
+        }
+        return new string(chars);
+    }
+}
