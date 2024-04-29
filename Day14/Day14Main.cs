f50575795ed79738201e4f06322be145e6c29abe
Olle Jörlid <a21olljo@student.his.se>
2024-04-29 20:18:13 +0200

    Day 14 init

---
+++ Day14/Day14Main.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+ * Ze Plan:
+ * Platform handler which sets up the platform as a list of lists of strings
+ * Where every string is the the column of the platform (input)
+ *      We are only interested in the vertical lines because we are only tilting the platform north    
+ * When platform set up, tilt platform by moving every O to the index+1 of previous # or O
+ * Then calculate weight by taking the list.count - index+1 of every O 
+ */
