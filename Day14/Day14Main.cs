01dc2ac568709d8f59fea5523360cb92a1fe6afe
Olle Jörlid <a21olljo@student.his.se>
2024-04-29 23:25:08 +0200

    Might be done with day 14 part 2. The algorithm does not have the best time complexity so I can't run it literally a billion times but it should work :)

--- Day14/Day14Main.cs
+++ Day14/Day14Main.cs
@@ -1,32 +1,32 @@
 ﻿using Day14;
-/*
- * Ze Plan:
- * Platform handler which sets up the platform as a lists of strings
- * Where every string is the the column of the platform (input)
- *      We are only interested in the vertical lines because we are only tilting the platform north    
- * When platform set up, tilt platform by moving every O to the index+1 of previous # or O
- * Then calculate weight by taking the list.count - index+1 of every O 
- */
-
 class Day14Main
 {
     public static async Task Main()
     {
         IEnumerable<string> inputs = File.ReadLines("C:\\Users\\olljo\\source\\repos\\AdventOfCode2023\\Day14\\Day14_input.txt");
         PlatformHandler platformHandler = new PlatformHandler();
+        List<string> platform = inputs.ToList();
 
-        List<string> verticalPlatform = platformHandler.SetUpPlatform(inputs);
-        List<string> tiltedVerticalPlatform = await platformHandler.TiltVerticalPlatform(verticalPlatform);
+        for (long i = 0; i < 1000000000; i++)
+        {
+            if (i%100000 == 0)
+                Console.WriteLine(1000000000 - i);    
+
+            platform = await platformHandler.TiltPlatformNorthAsync(platform);
+            platform = await platformHandler.TiltPlatformWestAsync(platform);
+            platform = await platformHandler.TiltPlatformSouthAsync(platform);
+            platform = await platformHandler.TiltPlatformEastAsync(platform);
+        }
 
         int weight = 0;
-        foreach (string verticalPattern in tiltedVerticalPlatform)
+        foreach (string plat in platform)
         {
-            Console.WriteLine(verticalPattern);
-            for (int i = 0; i < verticalPattern.Length; i++)
+            Console.WriteLine(plat);
+            for (int i = 0; i < plat.Length; i++)
             {
-                if (verticalPattern[i] == 'O')
+                if (plat[i] == 'O')
                 {
-                    weight = weight + verticalPattern.Length - i;
+                    weight = weight + plat.Length - i;
                 }
             }
         }
--- Day14/PlatformHandler.cs
+++ Day14/PlatformHandler.cs
@@ -7,39 +7,78 @@ using System.Threading.Tasks;
 namespace Day14;
 class PlatformHandler
 {
-    public List<string> SetUpPlatform(IEnumerable<string> inputs)
+    private List<string> FlipPattern(List<string> platform)
     {
         List<string> verticalPatterns = new List<string>();
-        List<string> inputList = inputs.ToList();
-        for (int i = 0; i < inputList.First().Length; i++)
+        for (int i = 0; i < platform.First().Length; i++)
         {
             StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < inputList.Count; j++)
+            for (int j = 0; j < platform.Count; j++)
             {
-                sb.Append(inputList[j][i]);
+                sb.Append(platform[j][i]);
             }
             verticalPatterns.Add(sb.ToString());
         }
         return verticalPatterns;
     }
 
-    public async Task<List<string>> TiltVerticalPlatform(List<string> verticalPlatform)
+    internal async Task<List<string>> TiltPlatformNorthAsync(List<string> platform)
     {
-        List<Task<string>> tasks = new List<Task<string>>();
+        List<string> verticalPlatform = FlipPattern(platform);
 
+        List<Task<string>> tasks = new List<Task<string>>();
         foreach (string pattern in verticalPlatform)
         {
-            tasks.Add(Task.Run(() => RollBoulders(pattern)));
+            tasks.Add(Task.Run(() => RollBouldersNorthWest(pattern)));
         }
         await Task.WhenAll(tasks);
 
-        List<string> tiltedVerticalPatterns = new List<string>();
-        tasks.ForEach(task => tiltedVerticalPatterns.Add(task.Result));
-
-        return tiltedVerticalPatterns;
+        List<string> tiltedPlatform = new List<string>();
+        tasks.ForEach(task => tiltedPlatform.Add(task.Result));
+        return FlipPattern(tiltedPlatform);
     }
+    internal async Task<List<string>> TiltPlatformWestAsync(List<string> platform)
+    {
+        List<Task<string>> tasks = new List<Task<string>>();
+        foreach (string pattern in platform)
+        {
+            tasks.Add(Task.Run(() => RollBouldersNorthWest(pattern)));
+        }
+        await Task.WhenAll(tasks);
 
-    private string RollBoulders(string pattern)
+        List<string> tiltedPlatform = new List<string>();
+        tasks.ForEach(task => tiltedPlatform.Add(task.Result));
+        return tiltedPlatform;
+    }
+    internal async Task<List<string>> TiltPlatformSouthAsync(List<string> platform)
+    {
+        List<string> verticalPlatform = FlipPattern(platform);
+
+        List<Task<string>> tasks = new List<Task<string>>();
+        foreach (string pattern in verticalPlatform)
+        {
+            tasks.Add(Task.Run(() => RollBouldersSouthEst(pattern)));
+        }
+        await Task.WhenAll(tasks);
+
+        List<string> tiltedPlatform = new List<string>();
+        tasks.ForEach(task => tiltedPlatform.Add(task.Result));
+        return FlipPattern(tiltedPlatform);
+    }
+    internal async Task<List<string>> TiltPlatformEastAsync(List<string> platform)
+    {
+        List<Task<string>> tasks = new List<Task<string>>();
+        foreach (string pattern in platform)
+        {
+            tasks.Add(Task.Run(() => RollBouldersSouthEst(pattern)));
+        }
+        await Task.WhenAll(tasks);
+
+        List<string> tiltedPlatform = new List<string>();
+        tasks.ForEach(task => tiltedPlatform.Add(task.Result));
+        return tiltedPlatform;
+    }
+    private string RollBouldersNorthWest(string pattern)
     {
         char[] chars = pattern.ToCharArray();
         bool boulderMoved = false;
@@ -73,6 +112,44 @@ class PlatformHandler
             if (boulderFound)
             {
                 chars[0] = 'O';
+            }
+        }
+        return new string(chars);
+    }
+    private string RollBouldersSouthEst(string pattern)
+    {
+        char[] chars = pattern.ToCharArray();
+        bool boulderMoved = false;
+        while (!boulderMoved)
+        {
+            bool boulderFound = false;
+            boulderMoved = true;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if ((chars[i] == '#' || chars[i] == 'O')
+                    && boulderFound)
+                {
+                    chars[i - 1] = 'O';
+                    boulderFound = false;
+                }
+
+                if (chars[i] == 'O' && !boulderFound)
+                {
+                    boulderFound = true;
+                    chars[i] = '.';
+                }
+                else
+                {
+                    if (boulderFound)
+                    {
+                        boulderMoved = false;
+                    }
+                    chars[i] = chars[i];
+                }
+            }
+            if (boulderFound)
+            {
+                chars[chars.Length - 1] = 'O';
             }
         }
         return new string(chars);
