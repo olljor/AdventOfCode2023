ffc846d44ae607815ffb303fe88ec01ba9c731d5
Olle Jörlid <a21olljo@student.his.se>
2024-05-02 18:19:49 +0200

    Sequence initialisation setup

--- Day15/Day15Main.cs
+++ Day15/Day15Main.cs
@@ -1,9 +1,17 @@
 ﻿
 
+using Day15;
+
 class Day15main
 {
     public static void Main()
     {
+        IEnumerable<string> inputs = File.ReadLines("C:\\Users\\olljo\\source\\repos\\AdventOfCode2023\\Day15\\Day15_input.txt");
 
+        Console.WriteLine(inputs.First());
+        SequenceHandler sequenceHandler = new SequenceHandler();
+        List<string> sequence = sequenceHandler.SetUpSequence(inputs);
+
+        Console.WriteLine(sequence.Count);
     }
 }
---
+++ Day15/SequenceHandler.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Day15;
+
+internal class SequenceHandler
+{
+    internal List<string> SetUpSequence(IEnumerable<string> inputs)
+    {
+        List<string> sequence = new List<string>();
+        foreach (var input in inputs)
+        {
+            sequence.AddRange(input.Split(','));
+        }
+        return sequence;
+    }
+}
